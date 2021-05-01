c1cd28177dfbba7b9b2f5e38d8da003358a3f262
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2021-05-01 18:09:07 +0200

    import version2

---
+++ sample.c
@@ -0,0 +1,28 @@
+#include "tree.h"
+#include "tree_aux.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (void)
+{
+  // make tree
+  printf ("make tree\n");
+  struct sample s = tree_sample ();
+
+  // print tree
+  tree_print_str (s.o);
+
+  printf ("tree height: %d\n", tree_height (s.o));
+  printf ("tree nb nodes: %d\n", tree_nnodes (s.o));
+
+  tree *r = tree_root (s.f);
+  printf ("tree root: \"%s\"\n", (char *)tree_value (r));
+
+  // tree free
+  printf ("tree free\n");
+  tree_free (s.o);
+
+  return EXIT_SUCCESS;
+}
---
+++ tree.h
@@ -0,0 +1,40 @@
+/**
+ * @brief Lightweight implementation of binary tree data structure.
+ **/
+
+#ifndef TREE_H
+#define TREE_H
+
+#include <stdbool.h>
+
+typedef struct tree_s tree;
+
+/* constructor */
+tree *tree_new (void *data);
+tree *tree_copy (tree *t);
+bool tree_equal (tree *t1, tree *t2);
+
+/* accessor */
+tree *tree_left (tree *t);
+tree *tree_right (tree *t);
+tree *tree_parent (tree *t);
+void *tree_value (tree *t);
+bool tree_is_leaf (const tree *t);
+bool tree_is_root (const tree *t);
+
+/* modificator */
+void tree_set_left (tree *t, tree *left);
+void tree_set_right (tree *t, tree *right);
+void tree_set_value (tree *t, void *data);
+
+/* misc */
+int tree_height (const tree *t);
+int tree_nnodes (const tree *t);
+tree *tree_root (tree *t);
+void tree_unlink (tree *t);
+
+/* destructor */
+void tree_free (tree *t);
+void tree_free_full (tree *t, void (*destroy) (void *));
+
+#endif
